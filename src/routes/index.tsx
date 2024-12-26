ce6b5cbaa33e192259ed0800e26483aa7dce828b
M Mon <0xmarkhydra@gmail.com>
2024-12-27 01:10:46 +0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Dashboard } from '../pages/Dashboard';
+import Dashboard from '../pages/Dashboard';
 import { BookList, BookCategories, BookIds } from '../pages/Books';
 import { UserManagement } from '../pages/Users';
 import { Questions, Exams } from '../pages/Warehouse';
