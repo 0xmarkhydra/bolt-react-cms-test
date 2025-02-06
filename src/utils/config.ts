adeb8573caea22451b957f02f63293c990066a74
M Mon <0xmarkhydra@gmail.com>
2025-02-07 02:53:36 +0700

    update

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || 'https://api.hsabook.vn';
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || 'https://hsa-education-backend-dev.up.railway.app';
 
 const CONFIG_APP = {
   API_ENDPOINT: API_ENDPOINT,
